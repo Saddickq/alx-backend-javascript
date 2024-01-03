9ab76fa5c4e7db344a58a69e8e54d615352fef44
Abubakr Saddiq <abubakrsaddickq@gmail.com>
2024-01-03 16:43:18 +0000

    fixed linter issues

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,6 +3,7 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
+  /* eslint-disable no-unused-vars */
     const task = true;
     const task2 = false;
   }
