fc67cfb7854a6c0558c0e03b509be54eaf6cc5f5
Abubakr Saddiq <abubakrsaddickq@gmail.com>
2024-01-03 16:11:46 +0000

    fixed requirement fail

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   var task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    let task = true;
+    let task2 = false;
   }
 
   return [task, task2];
