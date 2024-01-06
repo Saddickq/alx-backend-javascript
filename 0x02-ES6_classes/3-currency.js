2262a43bf14e3b9156bb0410595d141886808036
Abubakr Saddiq <abubakrsaddickq@gmail.com>
2024-01-06 22:48:36 +0000

    currency corrected

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -9,9 +9,6 @@ export default class Currency {
   }
 
   set code(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('code must be a string');
-    }
     this._code = value;
   }
 
@@ -20,9 +17,6 @@ export default class Currency {
   }
 
   set name(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('name must be a string');
-    }
     this._name = value;
   }
 
