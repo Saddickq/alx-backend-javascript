7bdcc148d08b45d0c02e8e25364edc05ab62a692
Abubakr Saddiq <abubakrsaddickq@gmail.com>
2024-01-06 22:40:36 +0000

    pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value === 'number') {
+      this._amount = value;
+    } else {
+      throw new TypeError('Amount must be a number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (value instanceof Currency) {
+      this._currency = value;
+    } else {
+      throw new TypeError('currency must be a Currency');
+    }
+  }
+
+  displayFullPrice() {
+    return (`${this._amount} ${this._currency._name} (${this._currency._code})`);
+  }
+}
