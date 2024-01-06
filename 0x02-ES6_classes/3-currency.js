9140d2cfc60d785f91a7a11c577a7712359d778c
Abubakr Saddiq <abubakrsaddickq@gmail.com>
2024-01-06 22:54:52 +0000

    currency corrected

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,7 +1,7 @@
 export default class Currency {
   constructor(code, name) {
-    this._code = code;
-    this._name = name;
+    this.code = code;
+    this.name = name;
   }
 
   get code() {
@@ -9,6 +9,9 @@ export default class Currency {
   }
 
   set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('code must be a string');
+    }
     this._code = value;
   }
 
@@ -17,6 +20,9 @@ export default class Currency {
   }
 
   set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('name must be a string');
+    }
     this._name = value;
   }
 
