ef9dda0bdd48cc7ac5547c5249570dc47324958e
Abubakr Saddiq <abubakrsaddickq@gmail.com>
2024-01-07 13:30:58 +0000

    fixed bug in 9-hoisting.js

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -29,20 +29,19 @@ export class StudentHolberton {
   }
 
   set holbertonClass(value) {
-      if (!(value instanceof HolbertonClass)) {
+    if (!(value instanceof HolbertonClass)) {
       throw new TypeError('holberton class must be of class holberton');
     }
     this._holbertonClass = value;
-}
+  }
 
-get fullStudentDescription() {
+  get fullStudentDescription() {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
-}
+  }
 }
 
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
-
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
