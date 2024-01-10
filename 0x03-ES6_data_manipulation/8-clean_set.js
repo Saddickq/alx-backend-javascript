c658cdd6e4f35585fdb2fd2c93ef42ece82927f8
Abubakr Saddiq <abubakrsaddickq@gmail.com>
2024-01-10 06:52:46 +0000

    clean set bug fixed

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,7 @@
 export default function cleanSet(set, startingString) {
-  if (!startingString || !set) return '';
+  if (!startingString || !set || !(set instanceof Set) || (typeof startingString !== 'string')) {
+    return '';
+  }
   return Array.from(set)
     .filter((item) => item.startsWith(startingString))
     .map((item) => item.replace(startingString, ''))
